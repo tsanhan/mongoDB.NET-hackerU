f7fde9bc2794c3ff046e6b93a630fdef39277ff8
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-07-18 23:06:14 +0300

    done

--- Program.cs
+++ Program.cs
@@ -9,10 +9,20 @@ namespace MongoDB.NET
         {
 
             MongoCRUD db = new MongoCRUD("AddressBook");
+            db.InserRecord("Users", new PersonModel { FirstName = "Victor", LastName = "Yampolsky" });
+            System.Console.WriteLine("done program");
             Console.ReadLine();
         }
     }
 
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
     public class MongoCRUD
     {
         private IMongoDatabase db;
@@ -22,6 +32,13 @@ namespace MongoDB.NET
             var client = new MongoClient();
             db = client.GetDatabase(database);
         }
+
+        public void InserRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
 
 
     }
