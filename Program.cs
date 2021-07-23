02b8273fdabef031bea34ca16fc03098abb037a2
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-07-23 15:53:18 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 
@@ -10,22 +12,37 @@ namespace MongoDB.NET
         {
 
             MongoCRUD db = new MongoCRUD("AddressBook");
-            PersonModel person = new PersonModel
-            {
-                FirstName = "VicVic",
-                LastName = "YamYam",
-                PrimaryAddress = new AddressModel
-                {
-                    city = "JLM",
-                    State = "ISL",
-                    StreetAddress = "Arie Ben Eliezer",
-                    ZipCode = "98321"
-                }
-            };
+            // PersonModel person = new PersonModel
+            // {
+            //     FirstName = "VicVic",
+            //     LastName = "YamYam",
+            //     PrimaryAddress = new AddressModel
+            //     {
+            //         city = "JLM",
+            //         State = "ISL",
+            //         StreetAddress = "Arie Ben Eliezer",
+            //         ZipCode = "98321"
+            //     }
+            // };
 
 
-            db.InserRecord("Users", person);
-            System.Console.WriteLine("done program");
+            // db.InserRecord("Users", person);
+            // System.Console.WriteLine("done program");
+
+            // foreach (var rec in recs)
+            // {
+            //     System.Console.WriteLine($"{rec.Id} {rec.FirstName} {rec.LastName}");
+            //     if (rec.PrimaryAddress != null)
+            //     {
+            //         System.Console.WriteLine(rec.PrimaryAddress.city);
+            //     }
+            //     System.Console.WriteLine();
+            // }
+
+
+            var recs = db.LoadRecords<PersonModel>("Users");
+            var oneRec = db.LoadRecordById<PersonModel>("Users", new Guid("240569c5-85ec-4fd7-99a5-5723ddfccea8"));
+
             Console.ReadLine();
         }
     }
@@ -51,6 +68,7 @@ namespace MongoDB.NET
 
 
     }
+
     public class MongoCRUD
     {
         private IMongoDatabase db;
@@ -65,8 +83,23 @@ namespace MongoDB.NET
         {
             var collection = db.GetCollection<T>(table);
             collection.InsertOne(record);
+        }
 
 
+        public List<T> LoadRecords<T>(string table)
+        {
+
+            var collection = db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
+            //new BsonDocument() => like select *
+            // and it a better way, no filtering/processing... depends on size (to big of a dataset it's not practicle)
+        }
+
+        public T LoadRecordById<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id); // it's not js, we need dynamic capabilities on unknown T data type 
+            return collection.Find(filter).First();
         }
 
 
